da05a987b0041a0397818227cf045fc1def74dca
Coyu123 <samkev2011@live.co.uk>
2026-03-22 19:23:34 +0000

    added config file

--- AnimeLibrary/MainWindow.xaml.cs
+++ AnimeLibrary/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
-using AnimeLibrary.View;
+﻿using AnimeLibrary.View;
 using AnimeLibrary.View.UserControls;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Windows;
 
 namespace AnimeLibrary
 {
@@ -10,13 +13,29 @@ namespace AnimeLibrary
         public MainWindow()
         {
             InitializeComponent();
+            bool IsValidDirectory(string path)
+            {
+                return Directory.Exists(path) && Directory.GetFiles(path).Length > 0;
+            }
 
+            string config = File.ReadAllText("config.json");
+            using JsonDocument jsonDoc = JsonDocument.Parse(config);
+
+            string directory = jsonDoc.RootElement.GetProperty("directory").GetString();
+
+            if (IsValidDirectory(directory))
+            {
+                Settings settings = new Settings();
+                settings.OpenDirectory(directory);
+                _ = LoadCards(Settings.animeDir.Keys.ToArray());
+            }
         }
 
         public async Task LoadCards(string[] ids)
         {
             var existingIds = AnimeCards.Children.OfType<AnimeCard>().Select(c => c.AnimeId).ToHashSet();
             tbSearch.Visibility = Visibility.Visible;
+            btnClearCards.Visibility = Visibility.Visible;
             var newIds = ids.Where(id => !existingIds.Contains(id)).ToList();
             var cardTasks = newIds.Select(async id =>
             {
@@ -73,6 +92,30 @@ namespace AnimeLibrary
             settings.Owner = this;
             settings.ShowDialog();
             windowDim.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnClearCards_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TrashHoverInfo.Visibility = Visibility.Visible;
+        }
+
+        private void btnClearCards_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            TrashHoverInfo.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnClearCards_Click(object sender, RoutedEventArgs e)
+        {
+            AnimeCards.Children.Clear();
+            tbSearch.Visibility = Visibility.Collapsed;
+            btnClearCards.Visibility = Visibility.Collapsed;
+
+            string config = File.ReadAllText("config.json");
+            JsonNode jsonNode = JsonNode.Parse(config);
+            jsonNode["directory"] = "";
+
+            File.WriteAllText("config.json", jsonNode.ToJsonString(new JsonSerializerOptions { WriteIndented = true }));
+            Settings.AnimeDirectory = null;
         }
     }
 }
--- AnimeLibrary/View/UserControls/Settings.xaml.cs
+++ AnimeLibrary/View/UserControls/Settings.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Win32;
 using System.Diagnostics;
 using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Automation;
 
 namespace AnimeLibrary.View.UserControls
 {
@@ -10,10 +15,26 @@ namespace AnimeLibrary.View.UserControls
     {
         public static string? AnimeDirectory { get; set; }
         public static Dictionary<string, string> animeDir = new Dictionary<string, string>();
+        public static bool SaveConfig { get; set; }
+        public static bool ResetConfig { get; set; }
 
         public Settings()
         {
             InitializeComponent();
+            bool IsValidDirectory(string path)
+            {
+                return Directory.Exists(path) && Directory.GetFiles(path).Length > 0;
+            }
+
+            string config = File.ReadAllText("config.json");
+            using JsonDocument jsonDoc = JsonDocument.Parse(config);
+
+            string directory = jsonDoc.RootElement.GetProperty("directory").GetString();
+
+            if (IsValidDirectory(directory))
+            {
+                AnimeDirectory = directory;
+            }
             txtDirectory.Text = AnimeDirectory;
         }
 
@@ -32,41 +53,73 @@ namespace AnimeLibrary.View.UserControls
                 _ = mainWindow.LoadCards(animeDir.Keys.ToArray());
                 mainWindow.cardScroll.ScrollToTop();
             }
+            if (SaveConfig)
+            {
+                string config = File.ReadAllText("config.json");
+                JsonNode jsonNode = JsonNode.Parse(config);
+                jsonNode["directory"] = AnimeDirectory;
+
+                File.WriteAllText("config.json", jsonNode.ToJsonString(new JsonSerializerOptions { WriteIndented = true }));
+            }
+            if (ResetConfig)
+            {
+                string config = File.ReadAllText("config.json");
+                JsonNode jsonNode = JsonNode.Parse(config);
+                jsonNode["directory"] = "";
+
+                File.WriteAllText("config.json", jsonNode.ToJsonString(new JsonSerializerOptions { WriteIndented = true }));
+            }
+
             Close();
         }
 
         private void btnBrowse_Click(object sender, RoutedEventArgs e)
         {
             OpenFolderDialog openFolderDialog = new OpenFolderDialog();
-            string idPattern = @"\((?<value>\d+)\)";
-            string idPattern2 = @"-\s*(?<value>\d+)";
             if (openFolderDialog.ShowDialog() == true)
             {
                 txtDirectory.Text = openFolderDialog.FolderName;
                 AnimeDirectory = openFolderDialog.FolderName;
-                string[] subFolderPaths = Directory.GetDirectories(openFolderDialog.FolderName);
-                foreach (string subFolderPath in subFolderPaths)
-                {
-                    string folderName = Path.GetFileName(subFolderPath);
-                    if (Regex.IsMatch(folderName, idPattern))
-                    {
-                        string id = Regex.Match(folderName, idPattern).Groups["value"].Value;
-                        if (!animeDir.ContainsKey(id))
-                        {
-                            animeDir.Add(id, subFolderPath);
-                        }
-                    }
-                    else if (Regex.IsMatch(folderName, idPattern2))
-                    {
-                        string id = Regex.Match(folderName, idPattern2).Groups["value"].Value;
-                        if (!animeDir.ContainsKey(id))
-                        {
-                            animeDir.Add(id, subFolderPath);
-                        }
-                    }
-
-                }
+                OpenDirectory(AnimeDirectory);
             }
+        }
+
+        public void OpenDirectory(string path)
+        {
+            string idPattern = @"\((?<value>\d+)\)";
+            string idPattern2 = @"-\s*(?<value>\d+)";
+            string[] subFolderPaths = Directory.GetDirectories(path);
+            foreach (string subFolderPath in subFolderPaths)
+            {
+                string folderName = Path.GetFileName(subFolderPath);
+                if (Regex.IsMatch(folderName, idPattern))
+                {
+                    string id = Regex.Match(folderName, idPattern).Groups["value"].Value;
+                    if (!animeDir.ContainsKey(id))
+                    {
+                        animeDir.Add(id, subFolderPath);
+                    }
+                }
+                else if (Regex.IsMatch(folderName, idPattern2))
+                {
+                    string id = Regex.Match(folderName, idPattern2).Groups["value"].Value;
+                    if (!animeDir.ContainsKey(id))
+                    {
+                        animeDir.Add(id, subFolderPath);
+                    }
+                }
+
+            }
+        }
+
+        private void chkSaveConfig_Checked(object sender, RoutedEventArgs e)
+        {
+            SaveConfig = true;
+        }
+
+        private void chkResetConfig_Checked(object sender, RoutedEventArgs e)
+        {
+            ResetConfig = true;
         }
     }
 }
